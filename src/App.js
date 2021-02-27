ae982941bc2f4923403e57a1a6b35cb814346a6b
github username <60378440+vmar13@users.noreply.github.com>
2021-02-27 16:07:09 -0500

    reverted appjs back to class comp

--- src/App.js
+++ src/App.js
@@ -1,62 +1,77 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import './App.css';
 import Person from './Person/Person';
 
-const App = (props) => {
-  const [ miGenteState, setMiGenteState ] = useState({
-    miGente: [
-        { nombre: 'Luis Miguel', trabajo: 'cantante' },
-        { nombre: 'Jorge Ramos', trabajo: 'periodista' },
-        { nombre: 'Frida Kahlo', trabajo: 'artista' }
-      ],
-      bolivia: 'La Paz'
-    });
+class App extends Component {
 
-  const cambiaNombreHandler = () => {
-    setMiGenteState({ miGente: [
-        { nombre: 'Romeo Santos', trabajo: 'cantante' },
-        { nombre: 'Maria Elena Salinas', trabajo: 'periodista' },
-        { nombre: 'Salvador Dalí', trabajo: 'artista' }
-      ]})
-      // this.setState({
-      //   miGente: [...this.state.miGente, {nombre: 'Salvador Dali', trabajo: 'artista'}]
-      // })
-      // console.log(this.state.miGente)
-    }
+  state = {
+  miGente: [
+    { nombre: 'Luis Miguel', trabajo: 'cantante' },
+    { nombre: 'Jorge Ramos', trabajo: 'periodista' },
+    { nombre: 'Frida Kahlo', trabajo: 'artista' }
+  ],
+  bolivia: 'La Paz'
+}
 
+cambiaNombreHandler = () => {
+  this.setState({ miGente: [
+    { nombre: 'Romeo Santos', trabajo: 'cantante' },
+    { nombre: 'Maria Elena Salinas', trabajo: 'periodista' },
+    { nombre: 'Salvador Dalí', trabajo: 'artista' }
+  ]})
+  // this.setState({
+  //   miGente: [...this.state.miGente, {nombre: 'Salvador Dali', trabajo: 'artista'}]
+  // })
+  // console.log(this.state.miGente)
+}
+  
 
-    // const { miGente } = this.state;
-    // const { cambiaNombreHandler } = this;
+  render() {
+    const { miGente } = this.state;
+    const { cambiaNombreHandler } = this;
     return (
       <div className="App">
         <button onClick={cambiaNombreHandler}>Cambia de Nombre</button>
-        <Person nombre={miGenteState.miGente[0].nombre} trabajo={miGenteState.miGente[0].trabajo} >Adivina cual soy</ Person>
-        <Person nombre={miGenteState.miGente[1].nombre} trabajo={miGenteState.miGente[1].trabajo} />
-        <Person nombre={miGenteState.miGente[2].nombre} trabajo={miGenteState.miGente[2].trabajo} />
+        <Person 
+          nombre={miGente[0].nombre} 
+          trabajo={miGente[0].trabajo} >Adivina cual soy</ Person>
+        <Person 
+          nombre={miGente[1].nombre} 
+          trabajo={miGente[1].trabajo} 
+          switchName={cambiaNombreHandler}/>
+        <Person 
+          nombre={miGente[2].nombre} 
+          trabajo={miGente[2].trabajo} />
       </div>
     );
 
+  }
 }
 
 export default App;
 
-// state = {
+// const [ miGenteState, setMiGenteState ] = useState({
 //   miGente: [
-//     { nombre: 'Luis Miguel', trabajo: 'cantante' },
-//     { nombre: 'Jorge Ramos', trabajo: 'periodista' },
-//     { nombre: 'Frida Kahlo', trabajo: 'artista' }
-//   ],
-//   bolivia: 'La Paz'
-// }
+//       { nombre: 'Luis Miguel', trabajo: 'cantante' },
+//       { nombre: 'Jorge Ramos', trabajo: 'periodista' },
+//       { nombre: 'Frida Kahlo', trabajo: 'artista' }
+//     ]
+//   });
 
-// cambiaNombreHandler = () => {
-//   this.setState({ miGente: [
-//     { nombre: 'Romeo Santos', trabajo: 'cantante' },
-//     { nombre: 'Maria Elena Salinas', trabajo: 'periodista' },
-//     { nombre: 'Salvador Dalí', trabajo: 'artista' }
-//   ]})
-//   // this.setState({
-//   //   miGente: [...this.state.miGente, {nombre: 'Salvador Dali', trabajo: 'artista'}]
-//   // })
-//   // console.log(this.state.miGente)
+// const [ bolivia, setBoliviaState ] = useState({ bolivia: 'La Paz' })
+
+// console.log(miGenteState, bolivia)
+
+// const cambiaNombreHandler = () => {
+//   setMiGenteState({ miGente: [
+//       { nombre: 'Romeo Santos', trabajo: 'cantante' },
+//       { nombre: 'Maria Elena Salinas', trabajo: 'periodista' },
+//       { nombre: 'Salvador Dalí', trabajo: 'artista' }
+//     ]
+//   })
+
+//     // setMiGenteState({
+//     //   miGente: [{nombre: 'Salvador Dali', trabajo: 'artista'}]
+//     // })
 // }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Person = (props) => {
-    const { nombre, trabajo, children } = props
+    const { nombre, trabajo, switchName, children } = props
 
     return (
         <div>
             <h4>{children}</h4>
-            <p>Me llamo {nombre} y soy un/a {trabajo}.</p>
+            <p onClick={switchName}>Me llamo {nombre} y soy un/a {trabajo}.</p>
         </div>
     )
 }
