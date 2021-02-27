38f9e0dd37b62612aab65c4cfc0a0a89351df97c
github username <60378440+vmar13@users.noreply.github.com>
2021-02-27 17:25:02 -0500

    added toggle miGente

--- src/App.js
+++ src/App.js
@@ -6,53 +6,63 @@ import Person from './Person/Person';
 class App extends Component {
 
   state = {
-  miGente: [
-    { nombre: 'Luis Miguel', trabajo: 'cantante' },
-    { nombre: 'Jorge Ramos', trabajo: 'periodista' },
-    { nombre: 'Frida Kahlo', trabajo: 'artista' }
-  ],
-  bolivia: 'La Paz'
+    miGente: [
+      { nombre: 'Luis Miguel', trabajo: 'cantante' },
+      { nombre: 'Jorge Ramos', trabajo: 'periodista' },
+      { nombre: 'Frida Kahlo', trabajo: 'artista' }
+    ],
+    bolivia: 'La Paz',
+    showMiGente: false
 }
 
-cambiaNombreHandler = (nuevoNombre) => {
-  this.setState({ 
-    miGente: [
-      { nombre: nuevoNombre, trabajo: 'cantante' },
-      { nombre: 'Maria Elena Salinas', trabajo: 'periodista' },
-      { nombre: 'Salvador Dalí', trabajo: 'artista' }
-  ]})
-  // this.setState({
-  //   miGente: [...this.state.miGente, {nombre: 'Salvador Dali', trabajo: 'artista'}]
-  // })
-  // console.log(this.state.miGente)
-}
+  cambiaNombreHandler = (nuevoNombre) => {
+    this.setState({ 
+      miGente: [
+        { nombre: nuevoNombre, trabajo: 'cantante' },
+        { nombre: 'Maria Elena Salinas', trabajo: 'periodista' },
+        { nombre: 'Salvador Dalí', trabajo: 'artista' }
+    ]})
+    // this.setState({
+    //   miGente: [...this.state.miGente, {nombre: 'Salvador Dali', trabajo: 'artista'}]
+    // })
+    // console.log(this.state.miGente)
+  }
   
-nombreDeFormHandler = (event) => {
-  this.setState({ 
-    miGente: [
-      { nombre: 'Max', trabajo: 'cantante' },
-      { nombre:  event.target.value, trabajo: 'periodista' },
-      { nombre: 'Salvador Dalí', trabajo: 'artista' }
-  ]})
-}
+  nombreDeFormHandler = (event) => {
+    this.setState({ 
+      miGente: [
+        { nombre: 'Max', trabajo: 'cantante' },
+        { nombre:  event.target.value, trabajo: 'periodista' },
+        { nombre: 'Salvador Dalí', trabajo: 'artista' }
+    ]})
+  }
+
+  toggleMiGente = () => {
+    const doesShow = this.state.showMiGente;
+    this.setState({ showMiGente: !doesShow })
+  }
 
   render() {
-    const { miGente } = this.state;
-    const { cambiaNombreHandler, nombreDeFormHandler } = this;
+    const { miGente, showMiGente } = this.state;
+    const { cambiaNombreHandler, nombreDeFormHandler, toggleMiGente } = this;
     return (
       <div className="App">
-        <button onClick={cambiaNombreHandler}>Cambia de Nombre</button>
-        <Person 
-          nombre={miGente[0].nombre} 
-          trabajo={miGente[0].trabajo} >Adivina cual soy</ Person>
-        <Person 
-          nombre={miGente[1].nombre} 
-          trabajo={miGente[1].trabajo} 
-          switchName={cambiaNombreHandler.bind(this, 'Max')}/>
-        <Person 
-          nombre={miGente[2].nombre} 
-          trabajo={miGente[2].trabajo}
-          changed={nombreDeFormHandler} />
+        <button onClick={toggleMiGente}>Show People</button>
+        { showMiGente ? 
+          <div>
+            <Person 
+              nombre={miGente[0].nombre} 
+              trabajo={miGente[0].trabajo} >Adivina cual soy</ Person>
+            <Person 
+              nombre={miGente[1].nombre} 
+              trabajo={miGente[1].trabajo} 
+              switchName={cambiaNombreHandler.bind(this, 'Max')}
+              changed={nombreDeFormHandler}/>
+            <Person 
+              nombre={miGente[2].nombre} 
+              trabajo={miGente[2].trabajo} />
+          </div> : null
+        }
       </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './Person.css';
 
 const Person = (props) => {
     const { nombre, trabajo, switchName, children, changed } = props
 
     return (
-        <div>
+        <div>  
+           
             <h4>{children}</h4>
-            <p onClick={switchName}>Me llamo {nombre} y soy un/a {trabajo}.</p>
-            <input type='text' onChange={changed} />
+
+            <div className='Person'>
+                <p onClick={switchName}>Me llamo {nombre} y soy un/a {trabajo}.</p>
+                <input type='text' onChange={changed} value={nombre} />
+            </div>
         </div>
     )
 }
