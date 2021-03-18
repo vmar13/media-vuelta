27d99b25e054873c5c7ec1c94a3be7675e7583cd
github username <60378440+vmar13@users.noreply.github.com>
2021-03-18 16:08:37 -0400

    created Char comp

---
+++ src/Validation/Char/Char.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Char = props => {
+
+    return (
+        <div className='Char'>
+            render Char comps here 
+        </div>
+    )
+}
+
+export default Char;
